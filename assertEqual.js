c8def43c22ad6c098bccc514b577f178da238bc3
Sharon Lee <sharonshlee@icloud.com>
2021-08-16 20:27:47 -0400

    Add assertEqual file.

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function (actual, expected) {
+  console.log(
+    actual === expected
+      ? `✅Assertion Passed: ${actual} === ${expected}`
+      : `❌Assertion Failed: ${actual} !=== ${expected}`
+  );
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 3);
