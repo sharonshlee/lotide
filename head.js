5d5340510b37c4add9e819ff2ec2258cb9cd7e51
Sharon Lee <sharonshlee@icloud.com>
2021-08-16 22:24:43 -0400

    edit operators

--- head.js
+++ head.js
@@ -2,7 +2,7 @@ const assertEqual = function (actual, expected) {
   console.log(
     actual === expected
       ? `✅Assertion Passed: ${actual} === ${expected}`
-      : `❌Assertion Failed: ${actual} !=== ${expected}`
+      : `❌Assertion Failed: ${actual} !== ${expected}`
   );
 };
 
