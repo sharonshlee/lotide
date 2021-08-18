950428ee6a2d98328698f01750289ba868c9fc69
Sharon Lee <sharonshlee@icloud.com>
2021-08-17 22:53:10 -0400

    implemented assert arrays equal.

---
+++ assertArraysEqual.js
@@ -0,0 +1,20 @@
+const eqArrays = function (a, b) {
+  let same = 0;
+  let different = 0;
+
+  for (let i = 0; i < a.length; i++) {
+    a[i] === b[i] ? same++ : different++;
+  }
+  return same === a.length;
+};
+
+const assertArraysEqual = function (arr1, arr2) {
+  return eqArrays(arr1, arr2)
+    ? `✅Assertion Passed: ${arr1} === ${arr2}`
+    : `❌Assertion Failed: ${arr1} !== ${arr2}`;
+};
+
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+console.log(assertArraysEqual([1, 2, 3], [3, 2, 1]));
+console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]));
+console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", 3]));
