71dcb6391b38fb6d2bc4e7425a9cf52b64f33c59
Sharon Lee <sharonshlee@icloud.com>
2021-08-18 23:50:41 -0400

    edit sort

--- eqObjects.js
+++ eqObjects.js
@@ -11,7 +11,9 @@ const eqArrays = function (a, b) {
     return false;
   }
   for (let i = 0; i < a.length; i++) {
-    if (a[i] !== b[i]) {
+    if (Array.isArray(a[i]) || Array.isArray(b[i])) {
+      return eqArrays(a[i], b[i]);
+    } else if (a[i] !== b[i]) {
       return false;
     }
   }
@@ -25,12 +27,15 @@ returns true or false, based on a perfect match.
 const eqObjects = function (object1, object2) {
   const arrKeys1 = Object.keys(object1);
   const arrKeys2 = Object.keys(object2);
-  const arrValues1 = Object.values(object1).sort((a, b) => a - b);
-  const arrValues2 = Object.values(object2).sort((a, b) => a - b);
+  const arrValues1 = Object.values(object1).sort(
+    ([a, b], [c, d]) => c - a || b - d
+  );
+  const arrValues2 = Object.values(object2).sort(
+    ([a, b], [c, d]) => c - a || b - d
+  );
 
   if (arrKeys1.length === arrKeys2.length) {
     if (eqArrays(arrValues1, arrValues2)) {
-      console.log(eqArrays(arrValues1, arrValues2));
       return true;
     }
   }
@@ -44,7 +49,17 @@ assertEqual(eqObjects(ab, ba), true); // => true
 const abc = { a: "1", b: "2", c: "3" };
 assertEqual(eqObjects(ab, abc), false); // => false
 
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false); // => false
+
 /*
+Output:
+✅Assertion Passed: true === true
+✅Assertion Passed: false === false
 ✅Assertion Passed: true === true
 ✅Assertion Passed: false === false
 */
