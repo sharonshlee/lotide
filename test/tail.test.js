4a24c369f6d10aa7a6e9629aabdb4ae07f5ee721
Sharon Lee <sharonshlee@icloud.com>
2021-08-23 19:00:57 -0400

    refactor tail using mocha and chai with modules

---
+++ test/tail.test.js
@@ -0,0 +1,15 @@
+const assert = require("chai").assert;
+const tail = require("../tail");
+
+describe("#tail", () => {
+  it(`returns [
+    "Lighthouse",
+    "Labs",
+  ]for ["Yo Yo", "Lighthouse", "Labs"]`, () => {
+    //assert.deepEqual for comparing arrays
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), [
+      "Lighthouse",
+      "Labs",
+    ]);
+  });
+});
